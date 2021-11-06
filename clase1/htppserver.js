8e4338379ffcca230d88b190bbb28b1bb7f2badb
carolinarozo <67168080+carolinarozo@users.noreply.github.com>
2021-11-05 22:14:39 -0500

    node.js

--- clase1/htppserver.js
+++ clase1/htppserver.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 http.createServer(function (req, res) {
-    console.log(res);
+    console.log(req);
     res.write("hola Mundo!!!!!!");
     res.end();
 }).listen(8080);
